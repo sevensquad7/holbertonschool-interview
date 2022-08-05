e030de3fa91431ef7c63c3f4343dd82e5f7b82bd
sevensquad7 <edgarxot@gmail.com>
2022-08-04 19:29:58 -0500

    load file lockboxed

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    newlist = []
+    k = len(boxes)
+    for i in boxes:
+        if len(i) == 0 and i is not boxes[k-1]:
+            return False
+        for j in i:
+            newlist.append(j)
+    for index, keys in enumerate(boxes):
+        if index in newlist or index < k-1:
+            return True
+        else:
+            return False
