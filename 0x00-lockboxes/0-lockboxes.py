725cb6d96602395bb105c444ef36adfd4e0810fc
sevensquad7 <edgarxot@gmail.com>
2022-08-07 12:30:09 -0500

    add comentary

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""Lockboxes Solutions"""
+
+
 
 def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
     newlist = []
-    k = len(boxes)
+    n = len(boxes)
     for i in boxes:
-        if len(i) == 0 and i is not boxes[k-1]:
+        if len(i) == 0 and i is not boxes[n-1]:
             return False
         for j in i:
             newlist.append(j)
     for index, keys in enumerate(boxes):
-        if index in newlist or index < k-1:
+        if index in newlist or index < n-1:
             return True
         else:
             return False
