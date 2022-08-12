7b90f6765d609798947ce1aed4f4c4e2aad0ab4f
sevensquad7 <edgarxot@gmail.com>
2022-08-11 19:58:39 -0500

    Insert in sorted linked list

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list.
+ * @head: double pointer of singly linked list.
+ * @number: value of node to insert.
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = NULL;
+
+	if ((*head) == NULL || (*head)->n >= number)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = number;
+		node->next = NULL;
+		if (*head)
+			node->next = (*head);
+		(*head) = node;
+		return (node);
+	}
+	else
+		return (insert_node(&((*head)->next), number));
+}
