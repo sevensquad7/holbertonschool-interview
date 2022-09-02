fffc67b73a4ad98e9f9045371b780ca476d77f15
sevensquad7 <edgarxot@gmail.com>
2022-09-01 20:34:27 -0500

    correction code

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,34 +1,84 @@
 #include "sandpiles.h"
 
 /**
- * grid_sum - sum 2 grids
- * @grid1: first matrix
- * @grid2: second matrix
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: 3x3 grid to computes
+ * @grid2: 3x3 grid to sum
+ *
+ * Return: Nothing
  */
-
-void grid_sum(int grid1[3][3], int grid2[3][3])
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0, res = 0;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			if (grid1[i][j] > 3)
+				res = 1;
+		}
+
+	while (res == 1)
+	{
+		if (res == 1)
+			print_grid1(grid1);
+		res = will_topple(grid1);
+	}
+}
+
+/**
+ * will_topple - Create a toppling round
+ * @grid1: 3x3 grid to computes
+ *
+ * Return: 1 its will topple, 0 otherwise
+ */
+int will_topple(int grid1[3][3])
+{
+	int i = 0, j = 0, bol = 0;
+	int grid2[3][3] = {
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0}
+	};
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
+			if (grid1[i][j] > 3)
+			{
+				grid1[i][j] -= 4;
+				if (i + 1 < 3)
+					grid2[i + 1][j] += 1;
+				if (i - 1 >= 0)
+					grid2[i - 1][j] += 1;
+				if (j + 1 < 3)
+					grid2[i][j + 1] += 1;
+				if (j - 1 >= 0)
+					grid2[i][j - 1] += 1;
+			}
 		}
 	}
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			if (grid1[i][j] > 3)
+				bol = 1;
+		}
+	return (bol);
 }
 
 /**
- * grid_print - print a grid
- * @grid: matrix
+ * print_grid1 - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
  */
-
-void grid_print(int grid[3][3])
+void print_grid1(int grid[3][3])
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
 	printf("=\n");
 	for (i = 0; i < 3; i++)
@@ -40,81 +90,5 @@ void grid_print(int grid[3][3])
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
-	}
-}
-
-/**
- * grid_is_ok - check if a grid is ok
- * @grid: matrix
- * Return: 1 if is ok or 0 if not
- */
-
-int grid_is_ok(int grid[3][3])
-{
-	int i = 0;
-	int j = 0;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (grid[i][j] > 3)
-				return (0);
-		}
-	}
-	return (1);
-}
-
-/**
- * grid_change - change a grid value
- * @grid1: matrix
- */
-
-void grid_change(int grid1[3][3])
-{
-	int i = 0;
-	int j = 0;
-	int gridx[3][3];
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-			gridx[i][j] = 0;
-	}
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (grid1[i][j] > 3)
-			{
-				grid1[i][j] = grid1[i][j] - 4;
-				if ((i - 1) >= 0 && (i - 1) < 3)
-					gridx[i - 1][j] += 1;
-				if ((j - 1) >= 0 && (j - 1) < 3)
-					gridx[i][j - 1] += 1;
-				if ((i + 1) >= 0 && (i + 1) < 3)
-					gridx[i + 1][j] += 1;
-				if ((j + 1) >= 0 && (j + 1) < 3)
-					gridx[i][j + 1] += 1;
-			}
-		}
-	}
-	grid_sum(grid1, gridx);
-}
-
-/**
- * sandpiles_sum - sum 2 sandpiles
- * @grid1: first matrix
- * @grid2: second matrix
- */
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
-{
-	grid_sum(grid1, grid2);
-	while (!grid_is_ok(grid1))
-	{
-		grid_print(grid1);
-		grid_change(grid1);
 	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,11 +1,10 @@
 #ifndef _SANDPILES_H_
 #define _SANDPILES_H_
+
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void grid_print(int grid[3][3]);
-int grid_is_ok(int grid[3][3]);
-void grid_change(int grid1[3][3]);
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid1(int grid[3][3]);
+int will_topple(int grid1[3][3]);
 
 #endif "_SANDPILES_H_"
