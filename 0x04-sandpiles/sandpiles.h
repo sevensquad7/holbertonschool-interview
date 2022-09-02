2de0cd229fafb8f7570673d1994554bfc19aada6
sevensquad7 <edgarxot@gmail.com>
2022-09-01 20:32:53 -0500

    adding prototypes

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -3,4 +3,9 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 #endif "_SANDPILES_H_"
