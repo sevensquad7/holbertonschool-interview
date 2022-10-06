ec578dd396075f98994a3970b4e5c4c3b3d9ad10
sevensquad7 <edgarxot@gmail.com>
2022-10-05 21:15:38 -0500

    changes conditionals

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,12 +4,34 @@
 """
 
 
-def validUTF8(data):
+def validUTF8(data: list) -> bool:
     """
     method that determines if a given data
     set represents a valid UTF-8 encoding
     """
-    for element in data:
-        if element > 255 or element < 1:
-            return False
-    return True
+    count = 0
+
+    for x in data:
+
+        if 191 >= x >= 128:
+
+            if not count:
+                return False
+
+            count -= 1
+        else:
+            if count:
+                return False
+
+            if x < 128:
+                continue
+            elif x < 224:
+                count = 1
+            elif x < 240:
+                count = 2
+            elif x < 248:
+                count = 3
+            else:
+                return False
+
+    return count == 0
