99844e0f1a22791979f395289d1bb4c565d0047c
sevensquad7 <edgarxot@gmail.com>
2022-10-18 00:05:40 -0500

    Slide line

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef _SLIDE_LINE_
+#define _SLIDE_LINE_
+
+#include <stdio.h>
+
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT -1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /* _SLIDE_LINE_ */
