a7f57135b06d519a223f6de7a0cbe0a3e1824a2d
sevensquad7 <edgarxot@gmail.com>
2022-12-02 00:23:53 -0500

    load change conditional

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,23 +7,24 @@ how many square units of water will be retained after it rains.
 
 
 def rain(walls):
-    """
-    walls is a list of non-negative integers.
-    """
-    arrposition = []
-    arrvalue = []
-    cont = 0
+  """
+  walls is a list of non-negative integers.
+  """
+  if not walls or len(walls) < 3:
+    return 0
+  arrposition = []
+  arrvalue = []
+  cont = 0
 
-    for it in range(len(walls)):
-        if walls[it] > 0:
-            arrposition.append(it)
-            arrvalue.append(walls[it])
-    for item in range(len(arrposition)-1):
-        if arrvalue[item] <= arrvalue[item+1]:
-            cont = cont + \
-                ((arrposition[item+1]-arrposition[item]-1)*arrvalue[item])
-
-        if arrvalue[item] > arrvalue[item+1]:
-            cont = cont + \
-                ((arrposition[item]-arrposition[item+1]-1)*arrvalue[item+1])
-    return(cont)
+  for it in range(len(walls)):
+    if walls[it] > 0:
+      arrposition.append(it)
+      arrvalue.append(walls[it])
+  for item in range(len(arrposition) - 1):
+    if arrvalue[item] <= arrvalue[item + 1]:
+      cont = cont + (
+        (arrposition[item + 1] - arrposition[item] - 1) * arrvalue[item])
+    else :
+      cont = cont + (
+        (arrposition[item + 1] - arrposition[item] - 1) * arrvalue[item + 1])
+  return(cont)
