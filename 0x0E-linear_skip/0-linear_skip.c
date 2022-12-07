0709e57be829a93ae533bc7ec210596d163e0060
sevensquad7 <edgarxot@gmail.com>
2022-12-06 19:01:24 -0500

    Linear search in skip list

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -13,34 +13,34 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *tmp = NULL;
+    skiplist_t *tmp = NULL;
 
-	if (list)
-	{
-		while (value > list->n && list->next)
-		{
-			tmp = list;
-			list = list->express;
-			if (!list)
-			{
-				list = tmp;
-				while (list->next)
-					list = list->next;
-			}
-			if (list != tmp->express)
-				break;
-			printf("Value checked at index [%ld] = [%d]\n", list->index,
-			       list->n);
-		}
-		printf("Value found between indexes [%ld] and [%ld]\n",
-		       tmp->index, list->index);
-		while (tmp && tmp->index <= list->index)
-		{
-			printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
-			if (value == tmp->n)
-				return (tmp);
-			tmp = tmp->next;
-		}
-	}
-	return (NULL);
+    if (list)
+    {
+        while (value > list->n && list->next)
+        {
+            tmp = list;
+            list = list->express;
+            if (!list)
+            {
+                list = tmp;
+                while (list->next)
+                    list = list->next;
+            }
+            if (list != tmp->express)
+                break;
+            printf("Value checked at index [%ld] = [%d]\n", list->index,
+                   list->n);
+        }
+        printf("Value found between indexes [%ld] and [%ld]\n",
+               tmp->index, list->index);
+        while (tmp && tmp->index <= list->index)
+        {
+            printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+            if (value == tmp->n)
+                return (tmp);
+            tmp = tmp->next;
+        }
+    }
+    return (NULL);
 }
