6b29dca9fba865bcdf6aa155f78ce716ae6df8f1
sevensquad7 <edgarxot@gmail.com>
2023-01-26 21:25:12 -0500

    load file rotate 2d matrix

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,17 @@ def rotate_2d_matrix(matrix):
     """
 
     sizeBucle = len(matrix[0])
-    arrTemp = matrix
+    arrTemp = []
+    count = 0
     for i in range(sizeBucle):
         for j in range(sizeBucle):
-            arrTemp[i][j] = matrix[j][i]
+            arrTemp.append(matrix[sizeBucle-1][i])
+            # print("{}-{}".format((sizeBucle-1),i))
+            # print(matrix[sizeBucle-1][i])
+            sizeBucle = sizeBucle-1
+        sizeBucle = len(matrix[0])
+
+    for row in range(sizeBucle):
+        for col in range(sizeBucle):
+            matrix[row][col] = arrTemp[count]
+            count += 1
